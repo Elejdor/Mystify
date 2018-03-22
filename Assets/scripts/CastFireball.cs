a2ca94a4bd83f60903a4f8a298c3de66f2731fad
jedrek <jedrekszor@wp.pl>
2018-03-22 20:28:20 +0100

    Zrobiłem castowanie fireballi, wiatru i flamethrowera. Lewy przycisk myszki to fireball, prawy to wind a razem to flamethrower. To średnio działa i pewnie się nie sprawdzi ale na razie tak jest. Nie robiłem jakiejś struktury plików, ktoś inny to zrobi lepiej. Na razie strzela w jedną stronę. Jak mam to jakoś poprawić to mówcie

---
+++ Assets/scripts/CastFireball.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CastFireball : MonoBehaviour {
+
+    public GameObject fireball;
+    public Vector2 velocity = new Vector2(5.0f, 0f);
+    public Vector2 offset = new Vector2(1.5f, 0f);
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+        if(Input.GetKeyDown(KeyCode.Mouse0) && !Input.GetKeyDown(KeyCode.Mouse1))
+        {
+            GameObject go = (GameObject)Instantiate(fireball, (Vector2)transform.position + offset, Quaternion.identity);
+            //Vector2 mousePosition = new Vector2(Camera.main.ScreenToWorldPoint(Input.mousePosition).x, Camera.main.ScreenToWorldPoint(Input.mousePosition).y);
+            Vector2 castPointPosition = new Vector2(0, 0);
+
+            go.GetComponent<Rigidbody2D>().velocity = new Vector2(velocity.x, velocity.y);
+            Destroy(go, 3f);
+
+        }
+	}
+}
---
+++ Assets/scripts/CastFlamethrower.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CastFlamethrower : MonoBehaviour {
+
+    public GameObject flamethrower;
+    public Vector2 offset = new Vector2(1.5f, 0f);
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (Input.GetKeyDown(KeyCode.Mouse0) && Input.GetKeyDown(KeyCode.Mouse1))
+        {
+            GameObject go = (GameObject)Instantiate(flamethrower, (Vector2)transform.position + offset, Quaternion.identity);
+            Destroy(go, 3f);
+
+        }
+
+    }
+}
