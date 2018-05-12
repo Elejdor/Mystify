a433498595f441cebe2cb98e6ab3275141c1aa43
elejdor <elejdor@gmail.com>
2018-05-12 14:07:01 +0200

    Simplified simple follow

--- Assets/scripts/SimpleCameraFollow.cs
+++ Assets/scripts/SimpleCameraFollow.cs
@@ -7,31 +7,16 @@ public class SimpleCameraFollow : MonoBehaviour {
     [SerializeField]
     Transform _target;
 
-    Vector2 _pos2;
+    Vector3 _offset;
+    float _speed = 0.1f;
 
-    float _maxSpeed = 8.0f;
-    float _threshold = 2.0f;
-    float _speed = 0.0f;
+    void Start()
+    {
+        _offset = _target.position - transform.position;
+    }
 
-	// Update is called once per frame
-	void Update () {
-        _pos2 = transform.position;
-        Vector2 targetPos2 = _target.position;
-        float dist = ( _pos2 - targetPos2 ).sqrMagnitude;
-        
-        if ( dist > _threshold )
-        {
-            _speed = (dist - _threshold ) * ( dist - _threshold );
-            if ( _speed > _maxSpeed )
-                _speed = _maxSpeed;
-        }
-        else
-        {
-            _speed = 0.0f;
-        }
-
-        Vector2 dir = targetPos2 - _pos2;
-        dir.Normalize();
-        transform.position += new Vector3( dir.x, dir.y, 0 ) * _speed * Time.deltaTime;
+    // Update is called once per frame
+    void Update () {
+        transform.position = Vector3.Lerp( transform.position, _target.position - _offset, _speed );
     }
 }
