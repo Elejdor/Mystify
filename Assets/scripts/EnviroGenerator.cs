5d59a2dc2f61ed86234880b5874046e8c98054bb
jedrek <jedrekszor@wp.pl>
2018-05-17 20:13:04 +0200

    fully operational enviro generator on main scene

--- Assets/scripts/EnviroGenerator.cs
+++ Assets/scripts/EnviroGenerator.cs
@@ -4,75 +4,163 @@ using UnityEngine;
 
 public class EnviroGenerator : MonoBehaviour {
 
+    public int downBottomRange;
+    public int downUpperRange;
+    public int midBottomRange;
+    public int midUpperRange;
+    public int upBottomRange;
+    public int upUpperRange;
+    public int stalagnatsBottomRange;
+    public int stalagnatsUpperRange;
+
     public List<GameObject> down;
     public List<GameObject> mid;
-    //public List<GameObject> up;
+    public List<GameObject> up;
+    public List<GameObject> stalagnats;
     public GameObject InstDown;
     public GameObject InstMid;
-    //public GameObject InstUp;
+    public GameObject InstUp;
     public GameObject InstGround;
+    public GameObject InstStal;
     public GameObject Ground;
     
     bool flagDown = true;
     bool flagMid = true;
     bool flagUp = true;
     bool flagGround = true;
+    bool flagStal = true;
     GameObject go;
-    float randPos;
+    int randPos;
     int randPref;
-    int randFlip = 1;
+    int randFlipX = 1;
+    int randFlipY = 1;
+    int randRot;
     Vector3 local;
     void Start () {
 		
 	}
 	
 	void Update () {
-        if ((InstDown.transform.position.x <= (transform.position.x + 12)) && flagDown==true)
+        if ((InstDown.transform.position.x <= (transform.position.x + 30)) && flagDown==true)               //down
         {
             do
             {
-                randFlip = Random.Range(-1, 2);
+                randFlipX = Random.Range(-1, 2);
             }
-            while (randFlip == 0);
-                
-            randPref = Random.Range(0, 5);
+            while (randFlipX == 0);
+
+            randPref = Random.Range(0, down.Count + 1);
+            randRot = Random.Range(-10, 11);
             go = Instantiate(down[randPref], InstDown.transform.position, Quaternion.identity);
-            go.transform.localScale = new Vector3(go.transform.localScale.x*randFlip, go.transform.localScale.y, go.transform.localScale.z);
+            go.transform.localScale = new Vector3(go.transform.localScale.x*randFlipX, go.transform.localScale.y, go.transform.localScale.z);
+            go.transform.Rotate(Vector3.forward * randRot);
+            if (go.GetComponent<SpriteRenderer>().sortingOrder == 2)
+                go.transform.parent = GameObject.Find("layer_0").transform;
 
+            else if (go.GetComponent<SpriteRenderer>().sortingOrder == -1)
+                go.transform.parent = GameObject.Find("layer_1").transform;
 
-            randPos = Random.Range(5f, 20f);
+            else if (go.GetComponent<SpriteRenderer>().sortingOrder == -2)
+                go.transform.parent = GameObject.Find("layer_2").transform;
+
+            else if (go.GetComponent<SpriteRenderer>().sortingOrder == -3)
+                go.transform.parent = GameObject.Find("layer_3").transform;
+
+            randPos = Random.Range(downBottomRange, downUpperRange + 1);
             InstDown.transform.position = new Vector3(InstDown.transform.position.x + randPos, InstDown.transform.position.y, InstDown.transform.position.z);
             flagDown = false;
         }
-        if ((InstMid.transform.position.x <= (transform.position.x + 12)) && flagMid == true)
+        if ((InstMid.transform.position.x <= (transform.position.x + 30)) && flagMid == true)               //mid
         {
-            while (randFlip == 0) randFlip = Random.Range(-1, 2);
-            randPref = Random.Range(0, 2);
+            do
+            {
+                randFlipX = Random.Range(-1, 2);
+            }
+            while (randFlipX == 0);
+
+            randPref = Random.Range(0, mid.Count + 1);
+            randRot = Random.Range(-10, 11);
             go = Instantiate(mid[randPref], InstMid.transform.position, Quaternion.identity);
-            local = go.transform.localScale;
-            local.x *= randFlip;
-            go.transform.localScale = local;
+            go.transform.localScale = new Vector3(go.transform.localScale.x * randFlipX, go.transform.localScale.y, go.transform.localScale.z);
+            go.transform.Rotate(Vector3.forward * randRot);
+            if (go.GetComponent<SpriteRenderer>().sortingOrder == 2)
+                go.transform.parent = GameObject.Find("layer_0").transform;
 
+            else if (go.GetComponent<SpriteRenderer>().sortingOrder == -1)
+                go.transform.parent = GameObject.Find("layer_1").transform;
 
-            randPos = Random.Range(34f, 62f);
+            else if (go.GetComponent<SpriteRenderer>().sortingOrder == -2)
+                go.transform.parent = GameObject.Find("layer_2").transform;
+
+            else if (go.GetComponent<SpriteRenderer>().sortingOrder == -3)
+                go.transform.parent = GameObject.Find("layer_3").transform;
+
+            randPos = Random.Range(midBottomRange, midUpperRange + 1);
             InstMid.transform.position = new Vector3(InstMid.transform.position.x + randPos, InstMid.transform.position.y, InstMid.transform.position.z);
             flagMid = false;
         }
-       /* if ((InstUp.transform.position.x <= (transform.position.x + 12)) && flagUp == true)
+         if ((InstUp.transform.position.x <= (transform.position.x + 30)) && flagUp == true)                //up
+         {
+             do
+             {
+                 randFlipX = Random.Range(-1, 2);
+             }
+             while (randFlipX == 0);
+
+            randPref = Random.Range(0, up.Count + 1);
+             randRot = Random.Range(-10, 11);
+             go = Instantiate(down[randPref], InstUp.transform.position, Quaternion.identity);
+             go.transform.localScale = new Vector3(go.transform.localScale.x * randFlipX, go.transform.localScale.y * -1, go.transform.localScale.z);
+             go.transform.Rotate(Vector3.forward * randRot);
+             if (go.GetComponent<SpriteRenderer>().sortingOrder == 2)
+                 go.transform.parent = GameObject.Find("layer_0").transform;
+
+             else if (go.GetComponent<SpriteRenderer>().sortingOrder == -1)
+                go.transform.parent = GameObject.Find("layer_1").transform;
+
+             else if (go.GetComponent<SpriteRenderer>().sortingOrder == -2)
+                go.transform.parent = GameObject.Find("layer_2").transform;
+
+             else if (go.GetComponent<SpriteRenderer>().sortingOrder == -3)
+                go.transform.parent = GameObject.Find("layer_3").transform;
+
+            randPos = Random.Range(upBottomRange, upUpperRange + 1);
+             InstUp.transform.position = new Vector3(InstUp.transform.position.x + randPos, InstUp.transform.position.y, InstUp.transform.position.z);
+             flagUp = false;
+         }
+        if ((InstStal.transform.position.x <= (transform.position.x + 30)) && flagStal == true)                //stalagnats
         {
-            while (randFlip == 0) randFlip = Random.Range(-1, 2);
-            randPref = Random.Range(0, 5);
-            go = Instantiate(down[randPref], InstUp.transform.position, Quaternion.identity);
-            local = go.transform.localScale;
-            local.x *= randFlip;
-            go.transform.localScale = local;
+            do
+            {
+                randFlipX = Random.Range(-1, 2);
+            }
+            while (randFlipX == 0);
 
+            do
+            {
+                randFlipY = Random.Range(-1, 2);
+            }
+            while (randFlipY == 0);
 
-            randPos = Random.Range(5f, 20f);
-            InstUp.transform.position = new Vector3(InstUp.transform.position.x + randPos, InstUp.transform.position.y, InstUp.transform.position.z);
-            flagUp = false;
-        }*/
-        if ((InstGround.transform.position.x <= (transform.position.x + 27)) && flagGround == true)
+            randPref = Random.Range(0, up.Count + 1);
+            //randRot = Random.Range(-5, 6);
+            go = Instantiate(stalagnats[randPref], InstStal.transform.position, Quaternion.identity);
+            go.transform.localScale = new Vector3(go.transform.localScale.x * randFlipX, go.transform.localScale.y * randFlipY, go.transform.localScale.z);
+            //go.transform.Rotate(Vector3.forward * randRot);
+            if (go.GetComponent<SpriteRenderer>().sortingOrder == -1)
+                go.transform.parent = GameObject.Find("layer_1").transform;
+
+            else if (go.GetComponent<SpriteRenderer>().sortingOrder == -2)
+                go.transform.parent = GameObject.Find("layer_2").transform;
+
+            else if (go.GetComponent<SpriteRenderer>().sortingOrder == -3)
+                go.transform.parent = GameObject.Find("layer_3").transform;
+
+            randPos = Random.Range(stalagnatsBottomRange, stalagnatsUpperRange + 1);
+            InstStal.transform.position = new Vector3(InstStal.transform.position.x + randPos, InstStal.transform.position.y, InstStal.transform.position.z);
+            flagStal = false;
+        }
+        if ((InstGround.transform.position.x <= (transform.position.x + 35)) && flagGround == true)             //ground
         {
             Instantiate(Ground, InstGround.transform.position, Quaternion.identity);
             InstGround.transform.position = new Vector3(InstGround.transform.position.x + 25.5f, InstGround.transform.position.y, InstGround.transform.position.z);
@@ -81,6 +169,7 @@ public class EnviroGenerator : MonoBehaviour {
         flagDown = true;
         flagMid = true;
         flagUp = true;
+        flagStal = true;
         flagGround = true;
     }
 }
