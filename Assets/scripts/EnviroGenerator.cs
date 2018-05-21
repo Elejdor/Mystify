4702ce4eff8b6f89d07f17233a60449931ad8b3f
J_Komputer\Jędrek <jedrekszor@wp.pl>
2018-05-21 11:11:42 +0200

    bug repairs of enviro generator

--- Assets/scripts/EnviroGenerator.cs
+++ Assets/scripts/EnviroGenerator.cs
@@ -4,25 +4,25 @@ using UnityEngine;
 
 public class EnviroGenerator : MonoBehaviour {
 
-    public int downBottomRange;
-    public int downUpperRange;
-    public int midBottomRange;
-    public int midUpperRange;
-    public int upBottomRange;
-    public int upUpperRange;
-    public int stalagnatsBottomRange;
-    public int stalagnatsUpperRange;
+    [SerializeField]int downBottomRange;
+    [SerializeField]int downUpperRange;
+    [SerializeField]int midBottomRange;
+    [SerializeField]int midUpperRange;
+    [SerializeField]int upBottomRange;
+    [SerializeField]int upUpperRange;
+    [SerializeField]int stalagnatsBottomRange;
+    [SerializeField]int stalagnatsUpperRange;
 
-    public List<GameObject> down;
-    public List<GameObject> mid;
-    public List<GameObject> up;
-    public List<GameObject> stalagnats;
-    public GameObject InstDown;
-    public GameObject InstMid;
-    public GameObject InstUp;
-    public GameObject InstGround;
-    public GameObject InstStal;
-    public GameObject Ground;
+    [SerializeField]List<GameObject> down;
+    [SerializeField]List<GameObject> mid;
+    [SerializeField]List<GameObject> up;
+    [SerializeField]List<GameObject> stalagnats;
+    [SerializeField]GameObject InstDown;
+    [SerializeField]GameObject InstMid;
+    [SerializeField]GameObject InstUp;
+    [SerializeField]GameObject InstGround;
+    [SerializeField]GameObject InstStal;
+    [SerializeField]GameObject Ground;
     
     bool flagDown = true;
     bool flagMid = true;
@@ -36,8 +36,16 @@ public class EnviroGenerator : MonoBehaviour {
     int randFlipY = 1;
     int randRot;
     Vector3 local;
+
+    GameObject layer_0;
+    GameObject layer_1;
+    GameObject layer_2;
+    GameObject layer_3;
     void Start () {
-		
+        layer_0 = GameObject.Find("layer_0");
+        layer_1 = GameObject.Find("layer_1");
+        layer_2 = GameObject.Find("layer_2");
+        layer_3 = GameObject.Find("layer_3");
 	}
 	
 	void Update () {
@@ -49,22 +57,22 @@ public class EnviroGenerator : MonoBehaviour {
             }
             while (randFlipX == 0);
 
-            randPref = Random.Range(0, down.Count + 1);
+            randPref = Random.Range(0, down.Count);
             randRot = Random.Range(-10, 11);
             go = Instantiate(down[randPref], InstDown.transform.position, Quaternion.identity);
             go.transform.localScale = new Vector3(go.transform.localScale.x*randFlipX, go.transform.localScale.y, go.transform.localScale.z);
             go.transform.Rotate(Vector3.forward * randRot);
             if (go.GetComponent<SpriteRenderer>().sortingOrder == 2)
-                go.transform.parent = GameObject.Find("layer_0").transform;
+                go.transform.parent = layer_0.transform;
 
             else if (go.GetComponent<SpriteRenderer>().sortingOrder == -1)
-                go.transform.parent = GameObject.Find("layer_1").transform;
+                go.transform.parent = layer_1.transform;
 
             else if (go.GetComponent<SpriteRenderer>().sortingOrder == -2)
-                go.transform.parent = GameObject.Find("layer_2").transform;
+                go.transform.parent = layer_2.transform;
 
             else if (go.GetComponent<SpriteRenderer>().sortingOrder == -3)
-                go.transform.parent = GameObject.Find("layer_3").transform;
+                go.transform.parent = layer_3.transform;
 
             randPos = Random.Range(downBottomRange, downUpperRange + 1);
             InstDown.transform.position = new Vector3(InstDown.transform.position.x + randPos, InstDown.transform.position.y, InstDown.transform.position.z);
@@ -78,22 +86,22 @@ public class EnviroGenerator : MonoBehaviour {
             }
             while (randFlipX == 0);
 
-            randPref = Random.Range(0, mid.Count + 1);
+            randPref = Random.Range(0, mid.Count);
             randRot = Random.Range(-10, 11);
             go = Instantiate(mid[randPref], InstMid.transform.position, Quaternion.identity);
             go.transform.localScale = new Vector3(go.transform.localScale.x * randFlipX, go.transform.localScale.y, go.transform.localScale.z);
             go.transform.Rotate(Vector3.forward * randRot);
             if (go.GetComponent<SpriteRenderer>().sortingOrder == 2)
-                go.transform.parent = GameObject.Find("layer_0").transform;
+                go.transform.parent = layer_0.transform;
 
             else if (go.GetComponent<SpriteRenderer>().sortingOrder == -1)
-                go.transform.parent = GameObject.Find("layer_1").transform;
+                go.transform.parent = layer_1.transform;
 
             else if (go.GetComponent<SpriteRenderer>().sortingOrder == -2)
-                go.transform.parent = GameObject.Find("layer_2").transform;
+                go.transform.parent = layer_2.transform;
 
             else if (go.GetComponent<SpriteRenderer>().sortingOrder == -3)
-                go.transform.parent = GameObject.Find("layer_3").transform;
+                go.transform.parent = layer_3.transform;
 
             randPos = Random.Range(midBottomRange, midUpperRange + 1);
             InstMid.transform.position = new Vector3(InstMid.transform.position.x + randPos, InstMid.transform.position.y, InstMid.transform.position.z);
@@ -107,22 +115,22 @@ public class EnviroGenerator : MonoBehaviour {
              }
              while (randFlipX == 0);
 
-            randPref = Random.Range(0, up.Count + 1);
+             randPref = Random.Range(0, up.Count);
              randRot = Random.Range(-10, 11);
-             go = Instantiate(down[randPref], InstUp.transform.position, Quaternion.identity);
+             go = Instantiate(up[randPref], InstUp.transform.position, Quaternion.identity);
              go.transform.localScale = new Vector3(go.transform.localScale.x * randFlipX, go.transform.localScale.y * -1, go.transform.localScale.z);
              go.transform.Rotate(Vector3.forward * randRot);
              if (go.GetComponent<SpriteRenderer>().sortingOrder == 2)
-                 go.transform.parent = GameObject.Find("layer_0").transform;
+                 go.transform.parent = layer_0.transform;
 
              else if (go.GetComponent<SpriteRenderer>().sortingOrder == -1)
-                go.transform.parent = GameObject.Find("layer_1").transform;
+                go.transform.parent = layer_1.transform;
 
              else if (go.GetComponent<SpriteRenderer>().sortingOrder == -2)
-                go.transform.parent = GameObject.Find("layer_2").transform;
+                go.transform.parent = layer_2.transform;
 
              else if (go.GetComponent<SpriteRenderer>().sortingOrder == -3)
-                go.transform.parent = GameObject.Find("layer_3").transform;
+                go.transform.parent = layer_3.transform;
 
             randPos = Random.Range(upBottomRange, upUpperRange + 1);
              InstUp.transform.position = new Vector3(InstUp.transform.position.x + randPos, InstUp.transform.position.y, InstUp.transform.position.z);
@@ -142,19 +150,19 @@ public class EnviroGenerator : MonoBehaviour {
             }
             while (randFlipY == 0);
 
-            randPref = Random.Range(0, up.Count + 1);
+            randPref = Random.Range(0, stalagnats.Count);
             //randRot = Random.Range(-5, 6);
             go = Instantiate(stalagnats[randPref], InstStal.transform.position, Quaternion.identity);
             go.transform.localScale = new Vector3(go.transform.localScale.x * randFlipX, go.transform.localScale.y * randFlipY, go.transform.localScale.z);
             //go.transform.Rotate(Vector3.forward * randRot);
             if (go.GetComponent<SpriteRenderer>().sortingOrder == -1)
-                go.transform.parent = GameObject.Find("layer_1").transform;
+                go.transform.parent = layer_1.transform;
 
             else if (go.GetComponent<SpriteRenderer>().sortingOrder == -2)
-                go.transform.parent = GameObject.Find("layer_2").transform;
+                go.transform.parent = layer_2.transform;
 
             else if (go.GetComponent<SpriteRenderer>().sortingOrder == -3)
-                go.transform.parent = GameObject.Find("layer_3").transform;
+                go.transform.parent = layer_3.transform;
 
             randPos = Random.Range(stalagnatsBottomRange, stalagnatsUpperRange + 1);
             InstStal.transform.position = new Vector3(InstStal.transform.position.x + randPos, InstStal.transform.position.y, InstStal.transform.position.z);
