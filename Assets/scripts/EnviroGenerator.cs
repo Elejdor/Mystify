622b296ac47db1c4b0948709bd349db04095648c
J_Komputer\Jędrek <jedrekszor@wp.pl>
2018-05-22 19:18:00 +0200

    further upgrades to the generator

--- Assets/scripts/EnviroGenerator.cs
+++ Assets/scripts/EnviroGenerator.cs
@@ -4,14 +4,10 @@ using UnityEngine;
 
 public class EnviroGenerator : MonoBehaviour {
 
-    [SerializeField]int downBottomRange;
-    [SerializeField]int downUpperRange;
-    [SerializeField]int midBottomRange;
-    [SerializeField]int midUpperRange;
-    [SerializeField]int upBottomRange;
-    [SerializeField]int upUpperRange;
-    [SerializeField]int stalagnatsBottomRange;
-    [SerializeField]int stalagnatsUpperRange;
+    [SerializeField]Vector2 downRange;
+    [SerializeField]Vector2 midRange;
+    [SerializeField]Vector2 upRange;
+    [SerializeField]Vector2 stalagnatsRange;
 
     [SerializeField]List<GameObject> down;
     [SerializeField]List<GameObject> mid;
@@ -30,7 +26,7 @@ public class EnviroGenerator : MonoBehaviour {
     bool flagGround = true;
     bool flagStal = true;
     GameObject go;
-    int randPos;
+    float randPos;
     int randPref;
     int randFlipX = 1;
     int randFlipY = 1;
@@ -63,7 +59,7 @@ public class EnviroGenerator : MonoBehaviour {
             go.transform.parent = layer_3.transform;
     }
 
-    void Generate(List<GameObject> list, int rot, GameObject Inst, int bottom, int upper, bool flag, bool flipY, bool up)
+    void getRand(bool flipY, List<GameObject> prefabs, int rot, Vector2 range)
     {
         randFlipX = Random.Range(0, 2);
         if (randFlipX == 0) randFlipX = -1;
@@ -72,9 +68,15 @@ public class EnviroGenerator : MonoBehaviour {
             randFlipY = Random.Range(0, 2);
             if (randFlipY == 0) randFlipY = -1;
         }
-        randPref = Random.Range(0, list.Count);
+        randPref = Random.Range(0, prefabs.Count);
         randRot = Random.Range(-rot, rot + 1);
-        go = Instantiate(list[randPref], Inst.transform.position, Quaternion.identity);
+        randPos = Random.Range(range.x, range.y + 1);
+    }
+
+    void Generate(List<GameObject> prefabs, int rot, GameObject InstPoint, Vector2 range, bool flag, bool flipY, bool up)
+    {
+        getRand(flipY, prefabs, rot, range);
+        go = Instantiate(prefabs[randPref], InstPoint.transform.position, Quaternion.identity);
         if (up == false)
             go.transform.localScale = new Vector3(go.transform.localScale.x * randFlipX, go.transform.localScale.y, go.transform.localScale.z);
 
@@ -83,24 +85,22 @@ public class EnviroGenerator : MonoBehaviour {
 
         go.transform.Rotate(Vector3.forward * randRot);
         sort(go);
-
-        randPos = Random.Range(bottom, upper + 1);
-        Inst.transform.position = new Vector3(Inst.transform.position.x + randPos, Inst.transform.position.y, Inst.transform.position.z);
+        InstPoint.transform.position = new Vector3(InstPoint.transform.position.x + randPos, InstPoint.transform.position.y, InstPoint.transform.position.z);
         flag = false;
     }
 	
 	void Update () {
         if ((InstDown.transform.position.x <= (transform.position.x + 30)) && flagDown==true) //down
-            Generate(down, 10, InstDown, downBottomRange, downUpperRange, flagDown, false, false);
+            Generate(down, 10, InstDown, downRange, flagDown, false, false);
         
         if ((InstMid.transform.position.x <= (transform.position.x + 30)) && flagMid == true) //mid
-            Generate(mid, 10, InstMid, midBottomRange, midUpperRange, flagMid, false, false);
+            Generate(mid, 10, InstMid, midRange, flagMid, false, false);
         
         if ((InstUp.transform.position.x <= (transform.position.x + 30)) && flagUp == true) //up
-            Generate(up, 10, InstUp, upBottomRange, upUpperRange, flagUp, false, true);
+            Generate(up, 10, InstUp, upRange, flagUp, false, true);
         
         if ((InstStal.transform.position.x <= (transform.position.x + 30)) && flagStal == true) //stalagnats
-            Generate(stalagnats, 0, InstStal, stalagnatsBottomRange, stalagnatsUpperRange, flagStal, true, false);
+            Generate(stalagnats, 0, InstStal, stalagnatsRange, flagStal, true, false);
         
         if ((InstGround.transform.position.x <= (transform.position.x + 35)) && flagGround == true) //ground
         {
