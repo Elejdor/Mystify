4f37128966ca337613081851de1bd4fff1762b86
Łukasz Nizik <elejdor@gmail.com>
2018-06-25 21:45:09 +0200

    Flamethrower - flame particles

--- Assets/scripts/Enemies/Golire.cs
+++ Assets/scripts/Enemies/Golire.cs
@@ -80,7 +80,6 @@ public class Golire : MonoBehaviour, IDamageable<float>
 
     public void Damage(float damage)
     {
-        Debug.Log("golireHP: " + _hp);
         _hp -= damage;
         if(_hp <= 0)
             death();
